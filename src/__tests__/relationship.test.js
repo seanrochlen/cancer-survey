96a85899fb49623f59a8b32f62ac2d9f70cb4561
Sean Rochlen <seanrochlen@gmail.com>
2020-05-04 16:17:00 -0500

    added relationship test

---
+++ src/__tests__/relationship.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Relationship from '../components/relationship';
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({t: key => key})
+}));
+
+const props = {
+  familyId: 0,
+  handleChange: jest.fn(),
+  person: { id: 12345, relationship: 'father' },
+};
+let wrapper;
+
+describe('<Relationship>', () => {
+  beforeEach(() => {
+    wrapper = mount(<Relationship {...props} />);
+  });
+  afterEach(() => {
+    wrapper.unmount();
+  });
+  describe('Events', () => {
+    test('Change - Select relationship - handleChange', () => {
+      wrapper.find('select').simulate('change', { target: { value : 'son'} } );
+      expect(wrapper.props().handleChange.mock.calls.length).toEqual(1);
+    });
+  });
+  describe('Rendering', () => {
+    test('select relationship name is relationship-${familyId}', () => {
+      expect(wrapper.find('select').props().name).toEqual('relationship-0');
+    });
+    test('select relationship value person.relationship', () => {
+      expect(wrapper.find('select').props().value).toBe('father');
+    });
+  });
+});
