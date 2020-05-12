fbb696038f352c6c385edddcfd2678618cd6039e
Sean Rochlen <seanrochlen@gmail.com>
2020-05-11 22:13:59 -0500

    added custom webpack

--- src/i18n.js
+++ src/i18n.js
@@ -2,12 +2,25 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+import translationEN from '../public/locales/en/translation.json';
+import translationES from '../public/locales/es/translation.json';
+
+const resources = {
+  en: {
+    translation: translationEN
+  },
+  es: {
+    translation: translationES
+  }
+};
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources,
+    lng: "en",
     fallbackLng: 'en',
     debug: true,
     interpolation: {
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const webpack = require("webpack");
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    }),
+    new webpack.ProgressPlugin(),
+  ]
+};
