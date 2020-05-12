38bf5349cbe39804875c835725ed9585f0d7eb61
Sean Rochlen <seanrochlen@gmail.com>
2020-05-11 22:16:15 -0500

    updated location of locales

--- src/i18n.js
+++ src/i18n.js
@@ -2,8 +2,8 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import translationEN from '../public/locales/en/translation.json';
-import translationES from '../public/locales/es/translation.json';
+import translationEN from './assets/locales/en/translation.json';
+import translationES from './assets/locales/es/translation.json';
 
 const resources = {
   en: {
